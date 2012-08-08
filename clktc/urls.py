4282b5e3ae139b8a6695255d4f62676f013fd086
hevok <age@liv.ac.uk>
2012-08-08 14:14:44 +0100

    Changed aws-django batch file to copy files from a folder instead of extracting from archive.

---
+++ clktc/urls.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+
+from django.contrib import admin
+admin.autodiscover()
+
+
+handler500 = "pinax.views.server_error"
+
+urlpatterns = patterns("")
+
+if settings.SERVE_MEDIA:
+    urlpatterns += patterns("",
+        url(r"", include("staticfiles.urls")),
+    )
+
+urlpatterns += patterns("",
+    url(r"^$", 'shorty.views.home', name='home'),
+    url(r"^e/([^/]+)/", 'shorty.views.manage', name='source'),
+    url(r"^a/", include(admin.site.urls)),
+    url(r"^(\w+)/", 'shorty.views.visit', name='visit'),
+)
+
+
