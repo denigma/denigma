f3e38045af84b72d4ed9e3b1f6f8bb1232a055a0
Hevok <age@liv.ac.uk>
2012-08-09 17:26:03 +0200

    Included the poll app.

--- clktc/settings.py
+++ clktc/settings.py
@@ -30,16 +30,33 @@ ADMINS = [
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
-        "NAME": "dev.db",                       # Or path to database file if using sqlite3.
-        "USER": "",                             # Not used with sqlite3.
-        "PASSWORD": "",                         # Not used with sqlite3.
-        "HOST": "",                             # Set to empty string for localhost. Not used with sqlite3.
-        "PORT": "",                             # Set to empty string for default. Not used with sqlite3.
+BACKEND = 'sqlite3'
+
+if BACKEND == 'sqlite3':
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
+            "NAME": "dev.db",                       # Or path to database file if using sqlite3.
+            "USER": "",                             # Not used with sqlite3.
+            "PASSWORD": "",                         # Not used with sqlite3.
+            "HOST": "",                             # Set to empty string for localhost. Not used with sqlite3.
+            "PORT": "",                             # Set to empty string for default. Not used with sqlite3.
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "mysql",
+            "NAME": 'clktc',
+            "USER": "root",
+            "PASWORD": "",
+            "HOST": "",
+            "PORT": "",
+            "OPTIONS": {
+                "init_command": "SET storage_engine = MYISAM",
+            }
+        }
+    }
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -145,6 +162,7 @@ INSTALLED_APPS = [
     
     # project
     "shorty",
+    "polls",
 ]
 
 FIXTURE_DIRS = [
