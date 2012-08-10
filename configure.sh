79093b5c7affb58e925728175ba5b4d4b9d2b63a
hevok <age@liv.ac.uk>
2012-08-10 15:59:49 +0200

    Corrected relative paths.

--- configure.sh
+++ configure.sh
@@ -3,6 +3,6 @@ echo "Configuring AMI: "
 
 sudo aptitude update; sudo aptitude -y safe-upgrade
 
-bash ./mysql-ebs /def/sdf
+sudo bash ./denigma/mysql-ebs /def/sdf
 
-bash ./init-db -D clktc -l https://github.com/hevok/denigma/raw/master/dump.sql
+sudo bash ./denigma/init-db -D clktc -l https://github.com/hevok/denigma/raw/master/dump.sql
