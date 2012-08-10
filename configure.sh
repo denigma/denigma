788ae4fe359ac7fd105e6107d1b3f708ad794c43
hevok <age@liv.ac.uk>
2012-08-10 15:48:50 +0200

    Removed cloning in configuration as well as made calls to relative paths, instead than to absolute paths.

--- configure.sh
+++ configure.sh
@@ -3,8 +3,6 @@ echo "Configuring AMI: "
 
 sudo aptitude update; sudo aptitude -y safe-upgrade
 
-git clone https://github.com/hevok/denigma.git
+bash ./mysql-ebs /def/sdf
 
-bash /home/ubuntu/denigma/mysql-ebs /def/sdf
-
-bash /home/ubuntu/denigma/init-db -D clktc -l https://github.com/hevok/denigma/raw/master/dump.sql
+bash ./init-db -D clktc -l https://github.com/hevok/denigma/raw/master/dump.sql
