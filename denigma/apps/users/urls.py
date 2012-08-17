a8a80ad9f8656c667a4e46d016e55b37c49467c3
Hevok <age@liv.ac.uk>
2012-08-17 15:17:08 +0200

    Renamed users to experts app in order to prevent collisions with the namespace.

--- denigma/apps/users/urls.py
+++
@@ -1,9 +0,0 @@
-"""Users urls."""
-from django.conf.urls.defaults import *
-
-
-urlpatterns = patterns('users.views',
-    (r'^whoiswho', 'whoiswho'),
-    (r'^list', 'list'),
-    (r'^', 'list')
-)
