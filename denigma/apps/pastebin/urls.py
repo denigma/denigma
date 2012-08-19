f5282f40b94efb9ffa21e609d03e6b426b7ab539
hevok <age@liv.ac.uk>
2012-08-19 11:02:06 +0200

    Included the import of url.

--- denigma/apps/pastebin/urls.py
+++ denigma/apps/pastebin/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns
+from django.conf.urls.defaults import patterns, url
 from django.views.generic.simple import direct_to_template
 
 from models import PastedItem
