fde5428e7e2a068a7aa8a721db2196be9c603d0d
hevok <age@liv.ac.uk>
2012-08-22 15:10:17 +0200

    Made a comment on the difference in the django template loader between version 1.3 and 1.4.

--- denigma/settings.py
+++ denigma/settings.py
@@ -127,7 +127,7 @@ SECRET_KEY = "$&li378l3_8wform1%!hphxb3_#bqomk!302kplo)16j1tp)z#"
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = [ # Redundant?
     "django.template.loaders.filesystem.Loader",
-    "django.template.loaders.app_directories.Loader",
+    "django.template.loaders.app_directories.Loader", # Depriciated in Django-1.4 and replaced by "django.template.laders.app_directorier.Loader".
     "django.template.loaders.filesystem.load_template_source",
     "django.template.loaders.app_directories.load_template_source",
 ]
