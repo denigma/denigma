283f29d7f5e801c62876a4d2a2a6309b25ab9527
hevok <age@liv.ac.uk>
2012-08-24 23:17:17 +0200

    Corrected path to settings for admin interface switching.

--- admin.py
+++ admin.py
@@ -1,7 +1,7 @@
 #! /user/bin/env python
 """Toggles the admin interface on server."""
 
-local_settings.py = "/home/denigma/denigma/local_settings.py"
+local_settings = "/home/denigma/denigma/local_settings.py"
 
 # Recieve settings and change debug values:
 input = open(local_settings, 'r').read()
