e41a9adeafca2b4159240f2461295e4eb903ecff
hevok <age@liv.ac.uk>
2012-08-25 14:47:23 +0200

    Extended the idea of multiple author blogs a bit.

--- denigma/apps/blogs/models.py
+++ denigma/apps/blogs/models.py
@@ -1,3 +1,12 @@
+"""This is ane experimental module to modelling mutliple author blogging.
+
+The djangopject weblog is an inspiration of this:
+https://www.djangoproject.com/weblog/
+
+For exisitng approaches see:
+http://www.djangopackages.com/grids/g/blogs/"""
+
+
 from django.db import models
 ##from djangotoolbox.fields import ListField
 ##
@@ -29,4 +38,35 @@ class Blog(models.Model):
     def __unicode__(self):
        return self.title
 
+
+"""Once user logs in, s/he can write a new comment (insert into comment table)
+or alternatively select the comment of some other user and give his feedback
+(insert into comment_feedback)
+Select a comment and write his/er own comment with a feedback on the original comment
+(inserted into comments table with root_comment as the orignal comment
+and inserted into the comment_feedback table for the original comment)"""
+
+#FEEDBACK_CHOICES = {
+#    (1, 'FOR'),
+#    (-1, 'AGAINST'),
+#    (0, 'NEUTRAL'),
+#}
+#
+#class User(models.Model): name = models.CharField(max_length=150)
+#from django.contrib.auth.models import User
+#
+#
+#class Comment(models.Model):
+#    text = models.CharField(max_length=1000)
+#    root_comment = models.ForeignKey('self', null=True, blank=True, related_name="children")
+#    written_by = models.ForeignKey(User)
+#
+#
+#class Comment_feedback(models.Model):
+#    feedback_user_id = models.ForeignKey(User)
+#    comment_id = models.ForeignKey(Comment)
+#    feedback_type_id = models.CharField(max_length=20, choices=FEEDBACK_CHOICES)
+#
+#    class Meta:
+#        unique_together = [("feedback_user_id", "info_id")]
 
