9716d4df73e644b82b34a38b8633bc176884d67f
hevok <age@liv.ac.uk>
2012-08-25 15:24:56 +0200

    Added a tutorial section and linked it to the what next page.

--- denigma/apps/about/urls.py
+++ denigma/apps/about/urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns("",
     url(r"^privacy/$", direct_to_template, {"template": "about/privacy.html"}, name="privacy"),
     url(r"^dmca/$", direct_to_template, {"template": "about/dmca.html"}, name="dmca"),
     url(r"^what_next/$", direct_to_template, {"template": "about/what_next.html"}, name="what_next"),
+    url(r"^tutorial/$", direct_to_template, {"template": "about/tutorial.html"}, name="tutorial"),
 )
