b1f3fce4ba2d05f9d3ff9f5d434332bab965e405
hevok <age@liv.ac.uk>
2012-08-29 11:50:13 +0200

    Reversed changes on the schmema of Blogs blog model for now.

--- denigma/apps/blogs/models.py
+++ denigma/apps/blogs/models.py
@@ -20,8 +20,8 @@ from django.db import models
 
 ##class Entry(models.Model):
 ##    published = models.BooleanField(db_index=True, default=True)
-##    created = models.DateField(auto_now_add=True)
-##    updated = models.DateField(auto_now=True)
+##    created = models.DateTimeField(auto_now_add=True)
+##    updated = models.DateTimeField(auto_now=True)
 ##    title = models.CharField(max_length=64)
 ##    text = models.TextField()
 ##
