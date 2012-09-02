52e89482676836540e28517a91ca31ea9ce6839d
hevok <age@liv.ac.uk>
2012-09-02 18:28:20 +0200

    Enabled the mapping of an article via its title.

--- denigma/apps/articles/urls.py
+++ denigma/apps/articles/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('articles.views',
     url(r'^(?P<pk>\d+)$', DetailView.as_view(
                               model=Post,
                               template_name="articles/view.html")),
+    url(r'^(?P<title>.+)$', 'view'),
 
     url(r'^', ListView.as_view(
                               queryset=Post.objects.filter(published=False).order_by("-created", "-id"),
--- denigma/apps/articles/views.py
+++ denigma/apps/articles/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+
+from blog.models import Post
+
+
+def view(request, title):
+    """Viewing article by title."""
+    post = Post.objects.get(title=title)
+    return render_to_response('articles/view.html', {'post': post})
