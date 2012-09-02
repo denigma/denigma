42e75d235fa4bc2c16867f721b0254bc27ed7998
hevok <age@liv.ac.uk>
2012-09-03 00:08:21 +0200

    Passed the request context to the template for the article by title mapping.

--- denigma/apps/articles/views.py
+++ denigma/apps/articles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 from blog.models import Post
 
@@ -6,4 +7,5 @@ from blog.models import Post
 def view(request, title):
     """Viewing article by title."""
     post = Post.objects.get(title=title.replace('_', ' ')) # Deslugify.
-    return render_to_response('articles/view.html', {'post': post})
+    return render_to_response('articles/view.html', {'post': post},
+                              context_instance=RequestContext(request))
