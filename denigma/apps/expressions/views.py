f9dbe8a3fe84adf5822183f325e0567ddfff2fb3
hevok <age@liv.ac.uk>
2012-09-05 21:23:21 +0200

    Included index view for expressions app.

--- denigma/apps/expressions/views.py
+++ denigma/apps/expressions/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def index(request):
+    return render_to_response('expressions/index.html',
+                             context_instance=RequestContext(request))
+
