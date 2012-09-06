efd3fdb4f9d6260793f2d9dfd726ceacbd318086
hevok <age@liv.ac.uk>
2012-09-06 18:24:08 +0200

    Filtered only published posts for indexing.

--- denigma/apps/blog/search_indexes.py
+++ denigma/apps/blog/search_indexes.py
@@ -15,4 +15,4 @@ class PostIndex(indexes.SearchIndex, indexes.Indexable):
 
     def index_queryset(self):
         """Used when the entire index for model is updated."""
-        return self.get_model().objects.filter(created__lte=datetime.datetime.now())
+        return self.get_model().objects.filter(created__lte=datetime.datetime.now(), published=True)
