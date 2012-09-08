e420ac1f4f4a9644d65677a44ce1c54ce595e9ff
hevok <age@liv.ac.uk>
2012-09-08 23:51:32 +0200

    Overwrote blog save method to allow saving referenced reStructured articles.

--- denigma/apps/blog/models.py
+++ denigma/apps/blog/models.py
@@ -1,9 +1,15 @@
-
+# -*- coding: utf-8 -*-
 from django.db import models
 from taggit.managers import TaggableManager
 
+try: 
+    from articles.manager import referencing
+except ImportError:
+   print("No article manager for references available.")
+
 
 class Post(models.Model):
+    """The fundamental textual datastructure of Denigma."""
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     published = models.BooleanField(db_index=True, default=True)
@@ -21,8 +27,19 @@ class Post(models.Model):
         return self.text.replace('\r', '')\
                         .replace('Abstract\n========', '')\
                         .replace('## Abstract', '')[:150] + '...'
+
     def slugify(self):
         return self.title.replace(' ', '_')
 
     def get_absolute_url(self):
         return '/blog/%i' % self.id
+
+    def save(self, *args, **kwargs):
+        """Triggers the generation of referened document if article is marked
+        with reStructured referenced."""
+        if self.text.startswith("reStructured referenced") and self.pk and\
+           "article" in [tag.name for tag in self.tags.all()]:
+            referencing(self)
+        super(Post, self).save(*args, **kwargs)
+
+#23456789112345678921234567893123456789412346789512345678961234567897123456789   
