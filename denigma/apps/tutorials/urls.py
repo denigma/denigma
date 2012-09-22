9d16fe0e01ac97734af8b0160e6417efdc26da7b
hevok <age@liv.ac.uk>
2012-09-23 00:25:03 +0200

    Moved OS-specific consideration to a development page in the tutorials app.

--- denigma/apps/tutorials/urls.py
+++ denigma/apps/tutorials/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import patterns, url
 
 
 urlpatterns = patterns('tutorials.views',
+    url('^$', 'index'),
     url('^(?P<tutorial_id>[^/]+)/view/$', 'view'),
     url('^(?P<tutorial_id>[^/]+)]/edit/$', 'edit'),
-    url('^$', 'index'),
-
+    url('^development/$', 'development', name='development'),
 )
--- denigma/apps/tutorials/views.py
+++ denigma/apps/tutorials/views.py
@@ -5,8 +5,10 @@ from blog.models import Post
 
 
 def index(request):
+    tutorials_entry = Post.objects.get(title="Tutorials")
     tutorials = Post.objects.filter(tags__name='tutorial').order_by('id')
-    return render_to_response('tutorials/index.html', {'tutorials': tutorials},
+    ctx = {'tutorial_entry': tutorials_entry, 'tutorials': tutorials}
+    return render_to_response('tutorials/index.html', ctx,
                               context_instance=RequestContext(request))
 
 def view(request, tutorial_id):
@@ -19,3 +21,12 @@ def edit(request, tutorial_id):
     tutiorial = Post.object.get(pk=tutorial_id)
     return render_to_response("./tutorials/view.html", {'tutorial': tutorial},
                              context_instance=RequestContext(request, {}))
+
+def development(request):
+    """Operation-specific developments considerations."""
+    windows = Post.objects.get(title="Windows")
+    mac = Post.objects.get(title="Mac")
+    linux = Post.objects.get(title="Linux")
+    ctx = {'windows': windows, 'mac': mac, 'linux': linux}
+    return render_to_response('tutorials/development.html', ctx,
+                              context_instance=RequestContext(request))
