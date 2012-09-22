7da517671d7a79be50caf563d3d186d56185dea8
hevok <age@liv.ac.uk>
2012-09-23 00:44:02 +0200

    Moved content out of the homepage and banner base into the database, included a dashboard and changed the last section of the banner homepage base.

--- denigma/views.py
+++ denigma/views.py
@@ -18,9 +18,23 @@ class SearchForm(forms.Form):
 def home(request):
     """The root source of all Denigmas URLs.
     Renders a dynamic home site with altered content."""
-    posts = Post.objects.filter(tags__name='news').order_by('-created', '-id')[:8] # Fetches all news.
-    searchform = SearchForm()
-    return render_to_response('homepage.html', {'posts': posts, 'searchform': searchform},
+    searchform = SearchForm() # Depricated?
+    denigma_description = Post.objects.get(title="Denigma Description")
+    denigma_rationality = Post.objects.get(title="Denigma Rationality")
+    news = Post.objects.filter(tags__name='news').order_by('-created', '-id')[:8] # Fetches all news.
+    research = Post.objects.get(title="Research")
+    programming = Post.objects.get(title="Programming")
+    design = Post.objects.get(title="Design")
+    dashboard = Post.objects.get(title="Dashboard")
+    ctx = {'searchform': searchform,
+           'denigma_description': denigma_description,
+           'denigma_rationality': denigma_rationality,
+           'posts': news,
+           'dashboard':dashboard,
+           'research': research,
+           'programming': programming,
+           'design': design}
+    return render_to_response('homepage.html', ctx,
                               context_instance=RequestContext(request))
 
 def search(request, term):
