3645e30d071ffe26733b94098ef644d281e9d731
hevok <age@liv.ac.uk>
2012-09-24 15:56:35 +0200

    Added Markdown basics link to the blog post edit view helper text.

--- denigma/apps/blog/forms.py
+++ denigma/apps/blog/forms.py
@@ -7,9 +7,10 @@ class PostForm(ModelForm):
     #title = CharField(widget=Textarea(attrs))
     text = CharField(widget=Textarea(attrs={'rows': 10, 'cols': 10,
                                             'style': 'font-family: monospace'}),
-    help_text='<a href="http://docutils.sourceforge.net/docs/user/rst/quickref.html=">reStructuredText Quick Reference</a>')
+    help_text='<a href="http://docutils.sourceforge.net/docs/user/rst/quickref.html=">reStructuredText Quick Reference</a>\
+     | <a href="daringfireball.net/projects/markdown/basics">Markdown Basics</a></p>')
 
-    comment = CharField(help_text='Optional, used for revisions control.', required=False)
+    comment = CharField(help_text='Optional, used for revision control.', required=False)
 
     class Meta:
         fields = ('title','text', 'tags', 'images', 'published', 'comment')
