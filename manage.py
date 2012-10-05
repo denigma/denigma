de9579407bc02265eb683a855f1258327c311c2d
hevok <age@liv.ac.uk>
2012-10-05 09:07:43 +0200

    Configured manage.py.

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys
+import os
 
 from os.path import abspath, dirname, join
 
@@ -16,7 +17,7 @@ from django.conf import settings
 from django.core.management import setup_environ, execute_from_command_line
 
 try:
-    import settings as settings_mod # Assumed to be in the same directory.
+    import denigma.settings as settings_mod # Assumed to be in the same directory.
 except ImportError:
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
     sys.exit(1)
@@ -29,4 +30,5 @@ setup_environ(settings_mod)
 sys.path.insert(0, join(settings.PROJECT_ROOT, "apps"))
 
 if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "denigma.settings")
     execute_from_command_line()
