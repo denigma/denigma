b488738b0965dca3621e9c0a7076301052287dfc
hevok <age@liv.ac.uk>
2012-10-06 12:17:33 +0200

    Added version identification to the project __init__.py

--- denigma/__init__.py
+++ denigma/__init__.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
+VERSION = (1, 4, 1, "f") # Following PEP 386.
+DEV_N = None
+
+
+def get_version():
+    version = "%s.%s" % (VERSION[0], VERSION[1])
+    if VERSION[2]:
+        version = "%s.%s" % (version, VERSION[2])
+    if VERSION[3] != "f":
+        version = "%s%s%s" % (version, VERSION[3], VERSION[4])
+        if DEF_N:
+            version = "%s.dev%s" % (version, DEV_N)
+    return version
+
+__version__ = get_version()
 
 __about__ = """
-This project lays the foundation for all other Pinax starter projects. It
+This project lays the foundation for deciphering life. It
 provides the project directory layout and some key infrastructure apps on
-which the other starter projects are based.
+which the other developments and enhancement of Denigma are based.
 """
