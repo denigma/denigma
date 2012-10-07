35368d7bedc8bb1fe5c1eff42f2ec309ab4dfb97
hevok <age@liv.ac.uk>
2012-10-07 16:28:44 +0200

    Improved the blog post model.

--- denigma/apps/blog/models.py
+++ denigma/apps/blog/models.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.db import models
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+#from django.utils.encoding import python_2_unicode_compatible
+
 from taggit.managers import TaggableManager
+
 
 try: 
     from articles.manager import referencing
@@ -10,15 +16,20 @@ except ImportError:
 
 class Post(models.Model):
     """The fundamental textual datastructure of Denigma."""
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    published = models.BooleanField(db_index=True, default=True)
-    title = models.CharField(max_length=250)
-    text = models.TextField()
-    tags = TaggableManager()
+    created = models.DateTimeField(_('created'), auto_now_add=True)
+    updated = models.DateTimeField(_('updated'), auto_now=True)
+    published = models.BooleanField(_('published'), db_index=True, default=True)
+    title = models.CharField(_('title'), max_length=250)
+    text = models.TextField(_('text'))
+    tags = TaggableManager(_('tags'))
+    #url = models.CharField(_('URL'), max_length=100, db_index=True, blank=True, null=True,
+    #    help_text="Example: '/future/projects/'. Make sure to have leading and trailing slashes.")
     images = models.ManyToManyField('gallery.PhotoUrl', blank=True)
 
     def __unicode__(self):
+        return self.title
+
+    def __str__(self):
         return self.title
 
     def brief(self):
@@ -32,7 +43,10 @@ class Post(models.Model):
         return self.title.replace(' ', '_')
 
     def get_absolute_url(self):
-        return '/blog/%i' % self.id
+        if hasattr(self, 'url') and self.url:
+            return self.url
+        else:
+            return '/blog/%i' % self.id
 
     def save(self, *args, **kwargs):
         """Triggers the generation of referened document if article is marked
@@ -40,6 +54,12 @@ class Post(models.Model):
         if self.text.startswith("reStructured referenced") and self.pk and\
            "article" in [tag.name for tag in self.tags.all()]:
             referencing(self)
+        if self.url.startswith('/'):
+            self.url = settings.BASE_URL + self.url
         super(Post, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = _('Entry')
+        verbose_name_plural = _('Entries')
 
 #23456789112345678921234567893123456789412346789512345678961234567897123456789   
