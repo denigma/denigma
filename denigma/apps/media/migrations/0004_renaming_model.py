1f29e2165f0b51d9ba67814ac4c5467c9cd2588a
hevok <age@liv.ac.uk>
2012-10-12 22:52:30 +0200

    Step 3. Add migration for renaming tables.

---
+++ denigma/apps/media/migrations/0004_renaming_model.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        db.rename_table('media_photourl', 'media_image')
+
+    def backwards(self, orm):
+        db.rename_table('media_image', 'media_photourl')
+
+    models = {
+        'media.image': {
+            'Meta': {'object_name': 'Image'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'uploaded': ('django.db.models.fields.DateTimeField', [], {}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        }
+    }
+
+    complete_apps = ['media']
