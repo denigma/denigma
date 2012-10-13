2eb25d4542abc00af745b1d9cdc27ef4e51f06f4
hevok <age@liv.ac.uk>
2012-10-13 08:51:19 +0200

    Included meta class for Image model with translated names.

--- denigma/apps/media/models.py
+++ denigma/apps/media/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+
 from django.db import models
 
+from django.utils.translation import ugettext_lazy as _
 
 class Image(models.Model):
     url = models.CharField(max_length=128)
@@ -15,3 +17,7 @@ class Image(models.Model):
 
     def name(self):
         return self.url.split('/')[-1].split('/')[0]
+
+    class Meta(models.Mode):
+        verbose_name = _('Image')
+        verbose_name_plural = _('Images')
