166aca6fceee0ef5a87133aa47e31001ef7ef006
hevok <age@liv.ac.uk>
2012-10-13 10:55:37 +0200

    Fetching now data entries for the tutorials and included seperation lines in the tutorials index view.

--- denigma/apps/tutorials/views.py
+++ denigma/apps/tutorials/views.py
@@ -1,32 +1,32 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-from blog.models import Post
+from data.models import Entry
 
 
 def index(request):
-    tutorials_entry = Post.objects.get(title="Tutorials")
-    tutorials = Post.objects.filter(tags__name='tutorial').order_by('id')
+    tutorials_entry = Entry.objects.get(title="Tutorials")
+    tutorials = Entry.objects.filter(tags__name='tutorial').order_by('id')
     ctx = {'tutorials_entry': tutorials_entry, 'tutorials': tutorials}
     return render_to_response('tutorials/index.html', ctx,
                               context_instance=RequestContext(request))
 
 def view(request, tutorial_id):
     print "Tutorial id is:", tutorial_id, type(tutorial_id)
-    tutorial = Post.objects.get(pk=tutorial_id)
+    tutorial = Entry.objects.get(pk=tutorial_id)
     return render_to_response("./tutorials/view.html", {'tutorial': tutorial},
                               context_instance=RequestContext(request, {}))
 
 def edit(request, tutorial_id):
-    tutiorial = Post.object.get(pk=tutorial_id)
+    tutorial = Entry.object.get(pk=tutorial_id)
     return render_to_response("./tutorials/view.html", {'tutorial': tutorial},
                              context_instance=RequestContext(request, {}))
 
 def development(request):
     """Operation-specific developments considerations."""
-    windows = Post.objects.get(title="Windows")
-    mac = Post.objects.get(title="Mac")
-    linux = Post.objects.get(title="Linux")
+    windows = Entry.objects.get(title="Windows")
+    mac = Entry.objects.get(title="Mac")
+    linux = Entry.objects.get(title="Linux")
     ctx = {'windows': windows, 'mac': mac, 'linux': linux}
     return render_to_response('tutorials/development.html', ctx,
                               context_instance=RequestContext(request))
