5e551206456ae2c46fec5de2a4c923120dfb84b5
hevok <age@liv.ac.uk>
2012-10-15 12:57:53 +0200

    Included blog post and data change indexes as the search can be specified.

--- denigma/apps/blog/search_indexes.py
+++ denigma/apps/blog/search_indexes.py
@@ -3,16 +3,16 @@ from haystack import indexes
 from models import Post
 
 
-#class PostIndex(indexes.SearchIndex, indexes.Indexable):
-#    created = indexes.DateTimeField(model_attr='created')
-#    updated = indexes.DateTimeField(model_attr='updated')
-#
-#    text = indexes.CharField(document=True, use_template=True)
-#    tags = indexes.MultiValueField()
-#
-#    def get_model(self):
-#        return Post
-#
-#    def index_queryset(self):
-#        """Used when the entire index for model is updated."""
-#        return self.get_model().objects.filter(created__lte=datetime.datetime.now(), published=True)
+class PostIndex(indexes.SearchIndex, indexes.Indexable):
+    created = indexes.DateTimeField(model_attr='created')
+    updated = indexes.DateTimeField(model_attr='updated')
+
+    text = indexes.CharField(document=True, use_template=True)
+    tags = indexes.MultiValueField()
+
+    def get_model(self):
+        return Post
+
+    def index_queryset(self):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.filter(created__lte=datetime.datetime.now(), published=True)
