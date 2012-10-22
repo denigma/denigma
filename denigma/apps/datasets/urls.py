5260ad0eff9adbcd5ac7d4003f2b1d444948376c
hevok <age@liv.ac.uk>
2012-10-22 22:00:05 +0200

    Added reference detail view.

--- denigma/apps/datasets/urls.py
+++ denigma/apps/datasets/urls.py
@@ -1,5 +1,8 @@
 """Dataset urls."""
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
+from django.views.generic import DetailView
+
+from models import Reference
 
 
 urlpatterns = patterns('datasets.views',
@@ -8,6 +11,7 @@ urlpatterns = patterns('datasets.views',
     url(r'^references/duplicates', 'duplicates'),
     url(r'^references/update', 'update_references'),
     url(r'^references', 'references'),
+    url(r'reference/(?P<pk>\d+)', DetailView.as_view(model=Reference), name='detail-reference'),
     url(r'^changes', 'changes', name='changes'),
     url(r'^epistasis', 'epistasis', name='epistasis'),
 )
