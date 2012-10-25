f7adb3acba6a57cdd846df1dbf3831cf0fa30b64
hevok <age@liv.ac.uk>
2012-10-25 14:32:57 +0200

    Changed filter article according to tags and categories.

--- denigma/apps/articles/urls.py
+++ denigma/apps/articles/urls.py
@@ -1,17 +1,21 @@
 from django.conf.urls import patterns,  url
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
 
 from data.models import Entry
 
 
 urlpatterns = patterns('articles.views',
     url(r'^(?P<pk>\d+)$', DetailView.as_view(
-                              model=Entry,
-                              context_object_name='article',
-                              template_name="articles/view.html")),
-    url(r'^(?P<title>.+)$', 'view'),
-
+            model=Entry,
+            context_object_name='article',
+            template_name="articles/view.html"),
+        name='article'),
+    url(r'^(?P<title>.+)$', 'view', name='article'),
     url(r'^', ListView.as_view(
-                              queryset=Entry.objects.filter(published=False).order_by("-created", "-id"),
-                              template_name="articles/index.html")),
+        queryset=Entry.objects.filter(
+            Q(tagged__name="article") |
+            Q(categories__name="Article")).order_by("-created", "-id"),
+        template_name="articles/index.html"),
+        name='articles'),
 )
