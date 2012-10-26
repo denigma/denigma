2e6e43d41df61d245584e4cedc24f0bc4d5a680f
hevok <age@liv.ac.uk>
2012-10-26 10:51:29 +0200

    Named tutorials and tutorials urls.

--- denigma/apps/tutorials/urls.py
+++ denigma/apps/tutorials/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import patterns, url
 
 
 urlpatterns = patterns('tutorials.views',
-    url('^$', 'index'),
-    url('^(?P<tutorial_id>[^/]+)/view/$', 'view'),
+    url('^$', 'index', name='tutorials'),
+    url('^(?P<tutorial_id>[^/]+)/view/$', 'view', name='tutorial'),
     url('^(?P<tutorial_id>[^/]+)]/edit/$', 'edit'),
     url('^development/$', 'development', name='development'),
 )
