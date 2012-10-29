aa0b1a972719bd04949d72dae0b9a45a0a0a0f0a
hevok <age@liv.ac.uk>
2012-10-29 21:39:41 +0100

    Enabled the idenifiction of lifespan factors in gene lists by ensembl gene identifiers.

--- denigma/apps/lifespan/templatetags/factor_linker.py
+++ denigma/apps/lifespan/templatetags/factor_linker.py
@@ -9,13 +9,15 @@ from lifespan.models import Factor
 register = template.Library()
 
 @register.filter
-def factor_links(value):
-    rc = re.compile('\d{4,}')
-    factors = dict([(str(factor.entrez_gene_id), factor) for factor in Factor.objects.all()])
-
+def factor_links(value, id='entrez_gene_id'):
+    if id == "entrez_gene_id":
+        rc = re.compile('\d{4,}')
+    else:
+        rc = re.compile('\S+')
+    factors = dict([(str(getattr(factor, id)), factor) for factor in Factor.objects.all()])
     def translate(match):
         if match.group(0) in factors:
-            print("lifespan.templatetag.factor_linker: Found factor %s" % match.group(0))
+            #print("lifespan.templatetag.factor_linker: Found factor %s" % match.group(0))
             return "<a href=%s>%s</a>" % (factors[match.group(0)].get_absolute_url(), match.group(0), )
         else:
             return match.group(0)
