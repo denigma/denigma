b72c394caabd1c23b4eb649ee2980f77283cb301
hevok <age@liv.ac.uk>
2012-11-18 14:49:51 +0100

    Reactivated TF-ID view.

--- denigma/apps/eva/urls.py
+++ denigma/apps/eva/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('eva.views',
     url('^$', 'index', name='eva'),
-    #url('^tf_idf/(?P<keyword>.+)', 'tf_idf', name='tf_idf')
+    url('^tf_idf/(?P<keyword>.+)', 'tf_idf', name='tf_idf')
 )
