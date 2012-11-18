355563c76aa0e9e0cf3de1102008cde58096fc87
hevok <age@liv.ac.uk>
2012-11-18 14:56:10 +0100

    Removed italization of 'omics' from the latinum template tag.

--- denigma/apps/articles/templatetags/latinum.py
+++ denigma/apps/articles/templatetags/latinum.py
@@ -5,7 +5,7 @@ from denigma.apps.data.models import Entry
 register = template.Library()
 
 LATIN = ['in silico', 'in vitro', 'in vivo', 'in situ',
-        'cis-', 'trans-', 'omics',
+        'cis-', 'trans-', # 'omics',
         'Saccharomyces cerevisae', 'S. cerevisae',
         'Caanorhabditis elegans', 'C. elegans',
         'Drosophila melanogaster', 'D. melanogaster',
