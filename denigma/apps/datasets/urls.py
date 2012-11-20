9a4c18da1e54f2088517d864509bf3f5e810f91c
hevok <age@liv.ac.uk>
2012-11-20 11:46:36 +0100

    Decorated reference update view with login required.

--- denigma/apps/datasets/urls.py
+++ denigma/apps/datasets/urls.py
@@ -1,16 +1,18 @@
 """Dataset urls."""
 from django.conf.urls import patterns, url
 from django.views.generic import DetailView
+from django.contrib.auth.decorators import login_required
 
 from models import Reference
 from views import ReferenceList, ReferenceCreate, ReferenceUpdate
+
 
 
 urlpatterns = patterns('datasets.views',
     url(r'^$', 'index', name='datasets'),
     url(r'^references/update', 'update_references'),
     url(r'^reference/create', ReferenceCreate.as_view(), name='create-reference'),
-    url(r'^reference/update/(?P<pk>\d+)', ReferenceUpdate.as_view(), name='update-reference'),
+    url(r'^reference/update/(?P<pk>\d+)', login_required(ReferenceUpdate.as_view()), name='update-reference'),
     url(r'^reference/autoupdate/(?P<pk>\d+)', 'autoupdate_reference', name='autoupdate_reference'),
     url(r'^references/duplicates', 'duplicates', name='duplicates'),
     url(r'^references/update', 'update_references'),
