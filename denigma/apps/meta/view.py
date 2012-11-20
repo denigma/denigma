d4232402e55a164058c3da10360551f7794a7cd8
hevok <age@liv.ac.uk>
2012-11-20 19:23:51 +0100

    Enabled the specification of the action flag.

--- denigma/apps/meta/view.py
+++ denigma/apps/meta/view.py
@@ -1,17 +1,20 @@
 """Log changes outside the admin"""
-from django.contrib.admin.models import LogEntry, ADDITION
+from django.contrib.admin.models import LogEntry
 from django.contrib.contenttypes.models import ContentType
 from django.utils.encoding import force_unicode
 
 
-def log(request, object, comment=None):
-    """Takes a request and a model object and generats a log entry."""
+def log(request, object, comment=None, action=1):
+    """Takes a request and a model object and generates a log entry.
+    Ab optional action flag can be provided:
+    actions = {1:'Addition', 2:'Change', 3:'Deletion'}
+    """
     LogEntry.objects.log_action(
         user_id = request.user.pk,
         content_type_id = ContentType.objects.get_for_model(object).pk,
         object_id = object.pk or object.id,
         object_repr = force_unicode(object),
-        action_flag = ADDITION,
+        action_flag = action,
         change_message = comment
     )
 
