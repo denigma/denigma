cc3c6ee30b9ab668a54853521d539de4b17e2500
hevok <age@liv.ac.uk>
2012-11-28 22:31:15 +0100

    Enabled creation of todos-quests conversion and quests solutions.

--- denigma/apps/quests/urls.py
+++ denigma/apps/quests/urls.py
@@ -2,10 +2,14 @@ from django.conf.urls import url, patterns
 
 from data.views import EntryView
 
+from views import CreateQuest, EngageQuest
+
 
 urlpatterns = patterns('quests.views',
     url(r'^$', 'index', name="quests"),
     url(r'^quest/(?P<slug>.+)', EntryView.as_view(
         template_name='quests/view.html',
-        context_object_name='quest'), name="quest")
+        context_object_name='quest'), name="quest"),
+    url(r'^engage/(?P<slug>.+)', EngageQuest.as_view(), name='engage-quest'),
+    url(r'^todo/(?P<pk>\d+)', CreateQuest.as_view(), name='create-quest'),
 )
--- denigma/apps/quests/views.py
+++ denigma/apps/quests/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.contrib.auth.models import User, AnonymousUser
+from django.utils.translation import ugettext as _
 
-from data.models import Entry
+
+from data.models import Entry, Category, Relation
+from data.views import Create
+from todos.models import Todo
+
+#from taggit.models import Tag
+
+import reversion
+
+from meta.view import log
 
 
 def index(request):
@@ -9,3 +22,63 @@ def index(request):
     quests = Entry.objects.get(title="Quests")
     return render(request, 'quests/index.html',
         {'entries': entries, 'quests': quests})
+
+
+class CreateQuest(Create):
+
+    def dispatch(self, request, *args, **kwargs):
+        self.todo = Todo.objects.get(pk=kwargs['pk'])
+        return super(CreateQuest, self).dispatch(request, *args, **kwargs)
+
+    def get_initial(self):
+        initial = super(CreateQuest, self).get_initial()
+        initial = initial.copy()
+        initial['title'] = self.todo.title
+        initial['text'] = self.todo.description
+        initial['categories'] = Category.objects.filter(name='Quest')
+        #initial['tags'] = ['todo']
+        return initial
+
+
+class EngageQuest(Create):
+
+    def dispatch(self, request, *args, **kwargs):
+        self.quest = Entry.objects.get(slug=kwargs['slug'])
+        del kwargs['slug']
+        return super(EngageQuest, self).dispatch(request, *args, **kwargs)
+
+    def get_initial(self):
+        initial = super(EngageQuest, self).get_initial()
+        initial = initial.copy()
+        #initial['title'] = 'engaged'
+        initial['parent'] = self.quest
+        #tag = Tag.objects.get_or_create(name='questing')
+        #initial['tags'] = self.quest.tags.all()#[tag]
+        return initial
+
+    def form_valid(self, form):
+        with reversion.create_revision():
+            self.object = form.save(commit=False)
+            if isinstance(self.request.user, AnonymousUser):
+                self.request.user = User.objects.get(username='Anonymous')
+            self.object.user = self.request.user
+            comment = self.request.POST['comment'] or self.comment
+            reversion.set_comment(comment)
+            self.object.comment = comment
+            self.object.save()
+            log(self.request, self.object, comment, 1)
+            comment = self.request.POST['comment'] or "Engaged quest"
+            relation = Relation(fr=self.quest, to=self.object, be=Entry.objects.get(title='solved by'))
+            relation.user = self.request.user
+            relation.save()
+            log(self.request, relation, comment, 1)
+            reversion.set_user(self.request.user)
+            form.save_m2m()
+            self.success_url = self.success_url or self.object.get_absolute_url()
+            messages.add_message(self.request, messages.SUCCESS,
+                _(self.message % self.object))
+            return HttpResponseRedirect(self.get_success_url())
+
+
+
+
