9605626c9cc753e1394139d9b7357188a16bf45f
hevok <age@liv.ac.uk>
2012-12-01 19:44:19 +0100

    Made the gallery text content dynamic.

--- denigma/apps/media/views.py
+++ denigma/apps/media/views.py
@@ -7,9 +7,8 @@ TODO:
   See: http://docs.amazonwebservices.com/AmazonS3/latest/dev/UsingAuthAccess.html
 """
 from django import forms
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.conf import settings
-from django.template import RequestContext
 
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
@@ -18,19 +17,25 @@ import mimetypes
 
 from models import Image
 
+try:
+    from data import get
+except ImportError:
+    pass
+
 
 class UploadForm(forms.Form):
     file = forms.ImageField(label='Select photo to upload')
 
+
 bucket = "dgallery"
 
-def index(request):
+def index(request, template='./gallery/index.html'):
+    entry = get("Gallery")
     def store_in_s3(filename, content):
-        pass
         conn = S3Connection(settings.ACCESS_KEY, settings.PASS_KEY)
         b = conn.create_bucket(bucket) # Bucket name
-        # if it doens't exist it creates a new one, otherweise it retrives it.
-        mime = mimetypes.guess_type(filename)[0] # Determines filetype.
+        # if it doesn't exist it creates a new one, otherwise it retrieves it.
+        mime = mimetypes.guess_type(filename)[0] # Determines file type.
         k = Key(b)
         k.key = filename
         k.set_metadata('Content-Type', mime)
@@ -40,14 +45,14 @@ def index(request):
     photos = Image.objects.all().order_by('-uploaded')
     if not request.method == "POST":
         f = UploadForm()
-        return render_to_response('./gallery/index.html', {'form':f, 'photos':photos},
-                                  context_instance=RequestContext(request))
+        ctx = {'entry': entry, 'form': f, 'photos': photos}
+        return render(request, template, ctx)
 
     f = UploadForm(request.POST, request.FILES)
     if not f.is_valid():
-        return render_to_response('./gallery/index.html', {'form':f, 'photos':photos},
-                                  context_instance=RequestContext(request))
-    
+        ctx = {'entry': entry, 'form': f, 'photos': photos}
+        return render(request, template, ctx)
+
     file = request.FILES['file']
     #print type(file)
     #print dir(file)
@@ -58,5 +63,8 @@ def index(request):
     p = Image(url="http://%s.s3.amazonaws.com/%s" % (bucket, file.name))
     p.save()
     photos = Image.objects.all().order_by('-uploaded')
-    return render_to_response('./gallery/index.html', {'form':f, 'photos':photos},
-                              context_instance=RequestContext(request))
+    ctx = {'entry': entry, 'form': f, 'photos': photos}
+    return render(request, template, ctx)
+
+#234567891123456789212345678931234567894123456789512345678961234567897123456789
+
