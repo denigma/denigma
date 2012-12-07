7eee6df9b658f70e551c52ce0d134700f90c0539
hevok <age@liv.ac.uk>
2012-12-07 17:22:19 +0100

    Improved regex for pubmed_linker by specifying leadning and trailing non-word characters.

--- denigma/apps/lifespan/templatetags/pubmed_linker.py
+++ denigma/apps/lifespan/templatetags/pubmed_linker.py
@@ -8,7 +8,7 @@ register = template.Library()
 
 @register.filter
 def pubmed_links(value):
-    rc = re.compile('[1-9]\d{6,}')
+    rc = re.compile('\W(?P<id>[1-9]\d{6,})\W')
     def translate(match):
         return "<a href=http://www.ncbi.nlm.nih.gov/pubmed/%s>%s</a>" % (match.group(0), match.group(0))
     return mark_safe(rc.sub(translate, value))
