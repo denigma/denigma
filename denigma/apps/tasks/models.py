d29c9e1cecf58276c140fcf273f8ca4af133c782
hevok <age@liv.ac.uk>
2012-12-08 15:50:19 +0100

    COmmented on the possibity to add a user/creator field to the Task model.

--- denigma/apps/tasks/models.py
+++ denigma/apps/tasks/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.sessions.models import Session
+#from django.contrib.auth.models import User
 
 
 class Task(models.Model):
     session = models.ForeignKey(Session)
     label = models.CharField(max_length=100)
     done = models.BooleanField(default=False)
+    #user/creator = models.ForeignKeyField(User)
 
     def __unicode__(self):
         return self.label
