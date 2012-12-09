fdd132cd10156da97449c5fbfba68c15c942af46
hevok <age@liv.ac.uk>
2012-12-09 20:54:47 +0100

    Enabled rendering of symbols in aspects level tables.

--- denigma/apps/aspects/tables.py
+++ denigma/apps/aspects/tables.py
@@ -4,7 +4,13 @@ from django.utils.safestring import mark_safe
 from models import Rank, Grade, Title
 
 
-class RankTable(tables.Table):
+class Table(tables.Table):
+
+    def render_symbol(self, value, record):
+        return mark_safe('<center><img src="%s" alt="%s" width="25" height="25"></center>' % (value.url, value.name))
+
+
+class RankTable(Table):
 
     def render_name(self, value, record):
         return mark_safe('<a href="/aspects/research/rank/%s">%s</a>' % (record.name, value))
@@ -15,7 +21,7 @@ class RankTable(tables.Table):
         exclude = ('id', 'type', 'hierarchy_ptr')
 
 
-class GradeTable(tables.Table):
+class GradeTable(Table):
 
     def render_name(self, value, record):
         return mark_safe('<a href="/aspects/programming/grade/%s">%s</a>' % (record.name, value))
