fcc89d22ec2d43fcae6db39943c11e218664c180
hevok <age@liv.ac.uk>
2012-12-12 10:25:44 +0100

    Undid the content links preveration in the negletion templatetag.

--- denigma/apps/blog/templatetags/negletion.py
+++ denigma/apps/blog/templatetags/negletion.py
@@ -28,11 +28,11 @@ def negle(value):
     .replace("'>o</a></b>", 'EnDcOnTeNt')
     return value.replace('<http://', '#~#')\
                 .replace('http://', "linkaging")\
-                .replace('#~#', '<http://')\
-                .replace("<b><a href='/data/entry/update/", '\n\nStArTcOnTeNt')\
-                .replace("'>o</a></b>", 'EnDcOnTeNt')#.replace('src="http://', 'linkimage').
+                .replace('#~#', '<http://')
+                #.replace("<b><a href='/data/entry/update/", '\n\nStArTcOnTeNt')\
+                #.replace("'>o</a></b>", 'EnDcOnTeNt')#.replace('src="http://', 'linkimage').
     # Leaving off the src enables urls in [] but disables plain urls.
-    # Exchaning of the middle replace by the commented out leads to the opposite effect
+    # Exchanging of the middle replace by the commented out leads to the opposite effect
 
 @register.filter
 def neglete(value):
@@ -41,10 +41,10 @@ def neglete(value):
     value = value.replace('&lt;', '<')\
          .replace('&quot;', '"')\
          .replace('&gt;', '>')\
-         .replace('linkaging', 'http://')\
-         .replace('\n\nStArTcOnTeNt', "<b><a href='/data/entry/update/", )\
-         .replace('</p>\n<p>StArTcOnTeNt', " <b><a href='/data/entry/update/")\
-         .replace('EnDcOnTeNt', "'>o</a></b>")  #.replace('linkimage', 'src="http://')\
+         .replace('linkaging', 'http://')
+         #.replace('\n\nStArTcOnTeNt', "<b><a href='/data/entry/update/", )\
+         #.replace('</p>\n<p>StArTcOnTeNt', " <b><a href='/data/entry/update/")\
+         #.replace('EnDcOnTeNt', "'>o</a></b>")  #.replace('linkimage', 'src="http://')\
     print(value)
     value = header4.sub(r"<h4>\1</h4>", value)
     value = header3.sub(r"<h3>\1</h3><p>", value)
