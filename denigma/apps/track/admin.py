30932538129670116fe8ffe62a9fc51b762aeeb5
hevok <age@liv.ac.uk>
2012-12-16 18:09:58 +0100

    Added location link to the track admin.

--- denigma/apps/track/admin.py
+++ denigma/apps/track/admin.py
@@ -4,8 +4,12 @@ from models import Visitor, BannedIP, UntrackedUserAgent
 
 
 class VisitorAdmin(admin.ModelAdmin):
-    list_display = ('session_key', 'ip_address', 'user', 'url', 'page_views', 'session_start', 'last_update')
+    list_display = ('session_key', 'ip_address', 'user', 'location', 'page_views', 'session_start', 'last_update')
 
+    def location(self, obj):
+        return '<a href="%s">%s</a>' % (obj.url, obj.url)
+
+    location.allow_tags = True
 
 admin.site.register(Visitor, VisitorAdmin)
 admin.site.register(BannedIP)
