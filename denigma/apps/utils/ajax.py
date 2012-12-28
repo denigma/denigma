712dde301eb7177988fc7cc9be4a7e3c76008eed
hevok <age@liv.ac.uk>
2012-12-28 18:15:22 +0100

    Included an ajax utility file.

---
+++ denigma/apps/utils/ajax.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.utils import simplejson
+from django.views.generic.detail import BaseDetailView, SingleObjectTemplateResponseMixin
+
+
+class JSONResponseMixin(object):
+    def render_to_reponse(self, context):
+        return self.get_json_response(self.convert_context_to_json(context))
+    def get_json_response(self, content, **httpresponse_kwargs):
+        return HttpResponse(content, content_type='application/json', **httpresponse_kwargs)
+    def convert_context_to_json(self, context):
+        return simplejson.dumps(context)
+
+
+class HybridDetailView(JSONResponseMixin, SingleObjectTemplateResponseMixin, BaseDetailView):
+    def render_to_response(self, context):
+        if self.request.is_ajax():
+            obj = context['object'].as_dict()
+            return JSONResponseMixin.render_to_response(self, obj)
+        else:
+            return SingleObjectTemplateResponseMixin.render_to_response(self, context)
+
