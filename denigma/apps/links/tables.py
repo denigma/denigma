34f22113d00c76f567237bb0988da7343ed17fc3
hevok <age@liv.ac.uk>
2012-12-29 12:57:21 +0100

    Rendered contact of link correctly.

--- denigma/apps/links/tables.py
+++ denigma/apps/links/tables.py
@@ -7,11 +7,24 @@ from models import Link
 
 
 class LinkTable(Table):
-    def render_title(self, value, record):
+    def render_title(self, record, value):
         return mark_safe('<a href="%s">%s' % (record.url, value))
 
     def render_description(self, value, record):
         return mark_safe('<a href="/links/update/%s">%s</a>' % (record.id, value))
+
+    def render_contact(self, record, value):
+        if value:
+            if "@" in value:
+                return mark_safe('<a href="mailto:%s">%s</a>' % (value, value.replace('@', '(at)')))
+            elif value.startswith('http'):
+                return mark_safe('<a href="%s">%s</a>' % (value, value.replace('www.', '').split('://')[-1]))
+            elif value.startswith('www.'):
+                return mark_safe('<a href="http://%s">%s</a>' % (value, value.split('www.')[-1]))
+            else:
+                return value
+        else:
+            return value
 
     class Meta:
         model = Link
