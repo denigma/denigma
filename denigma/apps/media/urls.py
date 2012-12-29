1f0164701a6834aebd20666e7bca6f6b7b6481b1
hevok <age@liv.ac.uk>
2012-12-29 02:59:12 +0100

    Added experimanteal slide show.

--- denigma/apps/media/urls.py
+++ denigma/apps/media/urls.py
@@ -1,4 +1,9 @@
-from django.conf.urls.defaults import patterns
+from django.conf.urls import patterns, url
+
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('media.views',
-    (r'^$', 'index'),)
+    url(r'^$', 'index', name='media'),
+    url(r'^slides', TemplateView.as_view(template_name='gallery/slides.html'), name='slides'),
+    url(r'^add/.+$', 'newImage'),
+)
