c1d980ed9012f56f6d40c24141b33f2542221793
hevok <age@liv.ac.uk>
2013-01-04 19:13:10 +0100

    Added types fields to the lifespan factor form.

--- denigma/apps/lifespan/forms.py
+++ denigma/apps/lifespan/forms.py
@@ -277,6 +277,7 @@ class FactorForm(ModelForm):
                 'maximum',
                 'pubmed_id',
                 'references',
+                'types',
                 'note',
                 'comment'
             ),
@@ -301,7 +302,7 @@ class FactorForm(ModelForm):
                   'taxid', 'species', 'observation', 'comment',
                   'assay', 'classifications', 'intervention',
                   'function', 'mean','median','maximum',
-                  'pubmed_id', 'references', 'note')
+                  'pubmed_id', 'references','types', 'note')
 #234567891123456789212345678931234567894123456789512345678961234567897123456789
 
 # Auxillary:
