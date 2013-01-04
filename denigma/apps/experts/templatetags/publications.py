8169f7770345813bfa86593befc26e0b1af7ac50
hevok <age@liv.ac.uk>
2013-01-05 00:47:56 +0100

    Established links from datasets reference authors to experts profiles.

--- denigma/apps/experts/templatetags/publications.py
+++ denigma/apps/experts/templatetags/publications.py
@@ -1,13 +1,17 @@
+"""Provides template tags for author reference mappings."""
 from django.template import Library
+from django.utils.safestring import mark_safe
 
 from Bio import Entrez
 Entrez.email = 'age@liv.ac.uk'
 
+from experts.models import Profile
 
 register = Library()
 
 @register.filter
 def medline(author):
+    """Fetches all publications of an author using the name."""
     results = {}
     name = "-".join([author.last_name, author.first_name[0]])
     r = Entrez.read(Entrez.esearch(db='pubmed', term=name))
@@ -15,3 +19,13 @@ def medline(author):
     for r in records:
         results[r['Id']] = r['Title']
     return results
+
+@register.filter
+def authors(names):
+    """Maps a list of names to experts profile if exists."""
+    names = names.split('; ')
+    experts = dict([(p.name_initials, p) for p in Profile.objects.all()])
+    for index, name in enumerate(names):
+        if name in experts:
+            names[index] = '<a href="%s">%s</a>' % (experts[name].get_absolute_url(), name)
+    return mark_safe("; ".join(names))
