c7f8f47b0f19822a0ebcd893c41736a61d628bf1
hevok <age@liv.ac.uk>
2013-01-13 13:45:19 +0100

    Added extra blank line between imports and models in expression models.

--- denigma/apps/expressions/models.py
+++ denigma/apps/expressions/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models import Q
 
+
 class Replicate(models.Model):
     #probe = models.ForeignKey('Probe')
     probe_id = models.CharField(max_length=255)
