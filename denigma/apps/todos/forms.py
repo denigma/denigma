d0c87c88c1d3d9cfc9669d4d633f8821ac7225ee
Hevok <age@liv.ac.uk>
2013-01-29 19:03:18 +0100

    Corrected import of user models into todos forms.

--- denigma/apps/todos/forms.py
+++ denigma/apps/todos/forms.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from django.forms import ModelForm, DateTimeField, ModelMultipleChoiceField
-from django.contrib.models import User
+from django.contrib.auth.models import User
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Fieldset, Submit
