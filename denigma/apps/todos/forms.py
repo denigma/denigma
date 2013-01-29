fe782242b508aca336ae87126efc25da30a1698d
Hevok <age@liv.ac.uk>
2013-01-29 18:49:08 +0100

    Reorderd ordering and displaying of todos attributes for form view.

--- denigma/apps/todos/forms.py
+++ denigma/apps/todos/forms.py
@@ -1,9 +1,13 @@
 from datetime import datetime
-from django.forms import ModelForm, DateTimeField
+from django.forms import ModelForm, DateTimeField, ModelMultipleChoiceField
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Fieldset, Submit
 from crispy_forms.bootstrap import FormActions
+
+from data.models import Entry
+from add.forms import MultipleSelectWithPop
+from profiles.models import Profile
 
 from models import Todo
 
@@ -11,6 +15,8 @@ from models import Todo
 class TodoForm(ModelForm):
     start_date = DateTimeField(initial=datetime.now(), required=False)
     stop_date = DateTimeField(initial=datetime.now(), required=False)
+    categories = ModelMultipleChoiceField(Entry.objects.all().order_by('title'), required=False, widget=MultipleSelectWithPop)
+    executor = ModelMultipleChoiceField(Profile.objects.all().order_by('name'), required=False)
     def __init__(self, *args, **kwargs):
         self.helper = FormHelper()
         self.helper.form_class = 'form-horizontal'
