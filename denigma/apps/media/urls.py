b050acda0c6e051185a7149fee6a20a029bdbc51
hevok <age@liv.ac.uk>
2013-01-30 01:21:50 +0100

    Updated the hierarchy of the media views and added prezi view.

--- denigma/apps/media/urls.py
+++ denigma/apps/media/urls.py
@@ -10,4 +10,5 @@ urlpatterns = patterns('media.views',
     url(r'^slides', TemplateView.as_view(template_name='gallery/slides.html'), name='slides'),
     url(r'^(?P<pk>\d+)', ImageView.as_view(), name='detail-image'),
     url(r'^delete/(?P<pk>\d+)', DeleteImage.as_view(), name='delete-image'),
+    url(r'^prezi', TemplateView.as_view(template_name='gallery/prezi.html'), name='prezi')
 )
