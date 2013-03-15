98d47313acb4578e9d45787642366f718ec55256
hevok <age@liv.ac.uk>
2013-03-15 18:04:09 +0100

    Transformed questionnarie fields into text fields.

--- denigma/apps/questionnaire/forms.py
+++ denigma/apps/questionnaire/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.utils.safestring import mark_safe
+from pagedown.widgets import PagedownWidget
 
 
 class SectionForm(forms.Form):
@@ -12,7 +14,11 @@ class SectionForm(forms.Form):
 
         for question in section.questions.all():
             choices = question.choices
-            kw = dict(help_text=question.question)
+            kw = dict(help_text=question.question, required=False, widget=PagedownWidget(
+                attrs={'rows': 1, 'cols': 10,
+                       'style': 'font-family: monospace',
+                       #'class':'wmd-input', 'id': 'wmd-input'
+                }))
 
             if choices:
                 fld = forms.ChoiceField
