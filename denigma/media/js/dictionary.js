bf73e34c35f5ee1b82f3da765111bd1f16afeab8
hevok <age@liv.ac.uk>
2013-03-19 17:57:17 +0100

    Updated dictionary for highlighting.

--- denigma/media/js/dictionary.js
+++ denigma/media/js/dictionary.js
@@ -9,7 +9,7 @@
       "Daniel": "Is founder of Denigma",
       "aging": "Will be defeated",
       "Crowd": "a large group of individuals capable of collective effort, in this case in the benefit of ageing research.",
-      "Citizen scientist": "is a representative of the crowd working in a research project.",
+      "Citizen scientists": "is a representative of the crowd working in a research project.",
       "Group intelligence": "is a capability delivered to science by the crowd through citizen scientists."
     };
     return $("p").annotate(dic, "term");
