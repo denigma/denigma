6f722a826dedbe20d59cac797b16e036c74492fc
hevok <age@liv.ac.uk>
2013-04-25 13:43:08 +0200

    Deleted build-in video app.

--- denigma/apps/video/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-
-class Comment(models.Model):
-    user = models.ForeignKey(User)
-    text = models.CharField(max_length=255)
-
