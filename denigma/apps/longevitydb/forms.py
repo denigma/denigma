231bd8a534d80ab91e125df827b8cdc252c229d6
hevok <age@liv.ac.uk>
2014-07-16 22:28:28 +0200

    Excluded none p-value records from lvdb search view

--- denigma/apps/longevitydb/forms.py
+++ denigma/apps/longevitydb/forms.py
@@ -1,6 +1,16 @@
 """Simple filter, might be obsolete"""
-from django.forms import (Form, BooleanField)
+from django.forms import (Form, BooleanField, ModelForm, MultipleChoiceField)
+
+# from lifespan.models import Variant
 
 
 class FilterForm(Form):
     output = BooleanField(required=False, initial=True)
+
+
+# class VariantForm(ModelForm):
+#     ethnicity = MultipleChoiceField()
+#
+#     class Meta:
+#         model = Variant
+#         fields = ('ethnicity')
