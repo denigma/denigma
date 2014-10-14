36a8e7982c6a8d73f64aff02380439b0c3b750b4
hevok <age@liv.ac.uk>
2014-10-14 13:37:30 -0700

    Fixed link to study type in longevity variant database.

--- denigma/apps/longevitydb/urls.py
+++ denigma/apps/longevitydb/urls.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import DetailView
 
-from lifespan.views import VarianceDetail, VariantDetail, FactorDetail, Population
+from lifespan.views import VarianceDetail, VariantDetail, FactorDetail, Population, StudyType
 
 from views import BrowseView, HomeView, AboutView #  SearchView,
 
@@ -22,10 +22,10 @@ urlpatterns = patterns('longevitydb.views',
     url(r'^legacy', TemplateView.as_view(template_name='longevitydb.html'),
         name='longevitydb'),
     url(r'^longevitydb', HomeView.as_view(), name='longevitydb-longevitydb'),
-    url(r'^detail/(?P<pk>\d+)/$', VariantDetail.as_view(template_name='longevitydb/detail.html'), name='variant'),
+    url(r'^detail/(?P<pk>\d+)/$', VariantDetail.as_view(template_name='longevitydb/detail.html'), name='lvdb-variant'),
     url(r'^factor_detail/(?P<pk>\d+)/$', FactorDetail.as_view(template_name='longevitydb/factor_detail.html'), name='lvdb-factor_detail'),
     url(r'^population_detail/(?P<pk>\d+)/$', DetailView.as_view(model=Population, template_name='longevitydb/population_detail.html'), name='lvdb-population_detail'),
-    url(r'^studytype_detail/(?P<pk>\d+)/$', DetailView.as_view(model=Population, template_name='longevitydb/studytype_detail.html'), name='lvdb-studytype_detail'),
+    url(r'^studytype_detail/(?P<pk>\d+)/$', DetailView.as_view(model=StudyType, template_name='longevitydb/studytype_detail.html'), name='lvdb-studytype_detail'),
     url(r'reference_detail/(?P<pk>\d+)', detail, {'template':'longevitydb/reference_detail.html'}, name='lvdb-reference_detail'),
 
 )
